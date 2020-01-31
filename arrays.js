771e94dc7e9f7a2b904242a3231f93a80b5ff094
Justin Evans <evansjustindavid@gmail.com>
2020-01-31 19:10:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,24 @@ function addElementToEndOfArray(a, b) {
 function accessElementInArray(a, b) {
   console.log (a[b]) 
 }
+
+
+function destructivelyRemoveElementFromBeginningOfArray(a) { 
+  return a.shift()
+  console.log destructivelyRemoveElementFromBeginningOfArray
+}
+
+function removeElementFromBeginningOfArray(a) {
+  return a.slice() 
+  console.log a 
+}
+
+function desctructivelyRemoveElementFromEndOfArray(a) {
+  return a.pop() 
+  console.log a 
+}
+
+function removeElementFromEndOfArray(a) { 
+  return a.slice(0, a.length - 1) 
+}
+
